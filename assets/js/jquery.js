804e61f3d72622816db65f5ec8e9f4b0dff37032
Aditya-Sinha-ADI <79050314+Aditya-Sinha-ADI@users.noreply.github.com>
2021-06-08 01:34:55 +0530

    anim-miss

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -12,6 +12,44 @@ $(document).ready(function($) {
       if ($('.navbar .navbar-toggler').attr('aria-expanded') == 'true' && clickOver.closest('.navbar').length === 0) {
               $('button[aria-expanded="true"]').click();
       }
+  });
+  $(function() {
+    var scroll1 = $("#nav");
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+      if (scroll >= 20) {
+        scroll1.addClass('scroll');
+      } else {
+        scroll1.removeClass('scroll');
+      }
+    });
+
+    var leftgear = document.getElementById("leftgear"),
+rightgear = document.getElementById("rightgear");
+    
+window.addEventListener("scroll", function() {
+    leftgear.style.transform = "rotate("+window.pageYOffset+"deg)";
+    rightgear.style.transform = "rotate(-"+window.pageYOffset+"deg)";
+});
+
+window.addEventListener("optimizedScroll", function() {
+  ;(function() {
+    var throttle = function(type, name, obj) {
+        var obj = obj || window;
+        var running = false;
+        var func = function() {
+            if (running) { return; }
+            running = true;
+            requestAnimationFrame(function() {
+                obj.dispatchEvent(new CustomEvent(name));
+                running = false;
+            });
+        };
+        obj.addEventListener(type, func);
+    };
+    throttle ("scroll", "optimizedScroll");
+})();
+})
     
     $('.mob-btn').click(function() {
       $(this).css('display', 'none');
