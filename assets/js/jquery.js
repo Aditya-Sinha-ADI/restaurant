3d7bf793e9bd1e1e5b96fddcb33236875bdce478
Aditya-Sinha-ADI <79050314+Aditya-Sinha-ADI@users.noreply.github.com>
2021-06-08 00:14:03 +0530

    bug-fx

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -12,62 +12,6 @@ $(document).ready(function($) {
       if ($('.navbar .navbar-toggler').attr('aria-expanded') == 'true' && clickOver.closest('.navbar').length === 0) {
               $('button[aria-expanded="true"]').click();
       }
-  });
-  $(function() {
-    var header1 = $("#image1");
-    var header2 = $("#image2");
-    var header3 = $("#image3");
-    var header4 = $("#image4");
-    var header5 = $("#image5");
-    var scroll1 = $("#nav");
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-      if (scroll >= 400) {
-      header1.addClass('nav-scrolled');
-      header2.addClass('nav-scrolled');
-      header3.addClass('nav-scrolled');
-      header4.addClass('nav-scrolled');
-      header5.addClass('nav-scrolled');
-      } else {
-        header1.removeClass('nav-scrolled');
-        header2.removeClass('nav-scrolled');
-        header3.removeClass('nav-scrolled');
-        header4.removeClass('nav-scrolled');
-        header5.removeClass('nav-scrolled');
-      }
-      if (scroll >= 20) {
-        scroll1.addClass('scroll');
-      } else {
-        scroll1.removeClass('scroll');
-      }
-    });
-
-    var leftgear = document.getElementById("leftgear"),
-rightgear = document.getElementById("rightgear");
-    
-window.addEventListener("scroll", function() {
-    leftgear.style.transform = "rotate("+window.pageYOffset+"deg)";
-    rightgear.style.transform = "rotate(-"+window.pageYOffset+"deg)";
-});
-
-window.addEventListener("optimizedScroll", function() {
-  ;(function() {
-    var throttle = function(type, name, obj) {
-        var obj = obj || window;
-        var running = false;
-        var func = function() {
-            if (running) { return; }
-            running = true;
-            requestAnimationFrame(function() {
-                obj.dispatchEvent(new CustomEvent(name));
-                running = false;
-            });
-        };
-        obj.addEventListener(type, func);
-    };
-    throttle ("scroll", "optimizedScroll");
-})();
-})
     
     $('.mob-btn').click(function() {
       $(this).css('display', 'none');
